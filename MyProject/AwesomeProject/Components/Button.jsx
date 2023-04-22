741c6762c792d6bf4607c1d1bde335d705297284
Harri <harri.kinsman@gmail.com>
2023-04-22 18:22:48 +0100

    Styled button round with an icon. Added function for on off colors

--- MyProject/AwesomeProject/Components/Button.jsx
+++ MyProject/AwesomeProject/Components/Button.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
-import { View, Button } from 'react-native';
-import Torch from 'react-native-torch';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, StyleSheet, StatusBar } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 function MyButton() {
+  const [isOn, setIsOn] = useState(false);
+
+  const handlePress = () => {
+    setIsOn(!isOn);
+  };
+
   return (
-    <View>
-      <Button title="Hello World" onPress={() => Torch.switchState(true)} />
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={handlePress}
+        style={[styles.button, { backgroundColor: isOn ? '#112240' : 'orange' }]}
+      >
+        <View style={styles.iconContainer}>
+          <MaterialCommunityIcons
+            name="power"
+            size={36}
+            color="#fff"
+          />
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: StatusBar.currentHeight + 350 || 0,
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    overflow: 'hidden',
+    backgroundColor: 'orange',
+    width: 100,
+    height: 100,
+  },
+  iconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default MyButton;
